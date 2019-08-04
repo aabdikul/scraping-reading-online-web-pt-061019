d1ba1bccc294ce6ae2311fc68a3b891ee963499f
Aida Abdikulova <abdikulova@gmail.com>
2019-08-04 03:28:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,4 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
-
-doc = Nokogiri::HTML(html)
-
-puts doc
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
